d91e6d1c81e469e8f6e0848b3b54cd0de34a1335
Kim Alex <kimtorres@gmail.com>
2022-01-15 16:51:40 -0300

    Corrigir html

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const imagem = document.querySelector('img');
 const botao = document.querySelector('button');
 const nomeDoPersonagem = document.querySelector('#nome');
-const especie = document.querySelector ('#especie')
-const estaVivo = document.querySelector('#status');
+const especie = document.querySelector ('#especie');
+const estaVivo = document.querySelector('#condicao');
+
+
+pegarPersonagem = () => {
+    return fetch(`https://rickandmortyapi.com/api/character/2`, {
+       method:'GET',
+       headers: {
+           Accept:'application/json',
+           "Content-type": 'application/json'
+       }
+    }).then((response) => response.json()).then((data) => {
+        imagem.src = data.image;
+        imagem.alt = data.name;
+        nomeDoPersonagem.innerHTML = data.name;
+        especie.innerHTML = data.species;
+        condicao.innerHTML = data.status;
+    });
+}
+
+botao.onclick = pegarPersonagem;
